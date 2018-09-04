686664c01e536073f1c7814c4296e549584ab7c7
Noam Amit <noamamit92@gmail.com>
2018-09-04 16:16:33 +0300

    convert to central package when the actula binding to the feature will be done in the view pacakge

--- js/prm-full-view-controller.js
+++ js/prm-full-view-controller.js
@@ -7,10 +7,10 @@ Note:
 is in development and can be added once completed
 */
 
-app.component('prmFullViewServiceContainerAfter', {
+/*app.component('prmFullViewServiceContainerAfter', {
     bindings: {parentCtrl: '<'},
     template: `
                <julac-link-to-hkall parent-ctrl="$ctrl.parentCtrl"></julac-link-to-hkall>
                <julac-view-it-from-other-inst parent-ctrl="$ctrl.parentCtrl"></julac-view-it-from-other-inst>
     `
-});
+});*/
