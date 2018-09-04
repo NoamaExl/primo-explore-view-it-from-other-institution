741f0fdd07d740e7a047a89931ea3f4a4c59fffe
Noam Amit <noamamit92@gmail.com>
2018-09-04 16:46:13 +0300

    update custom.js

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,7 @@
 "use strict";
 'use strict';
 
+ 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 var app = angular.module('centralCustom', ['angularLoad']);
